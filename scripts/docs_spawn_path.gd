e7394d73206d8278025e2107bd8baa9b10ed2211
dofiki <dixitregmiofficial@gmail.com>
2026-06-22 19:58:45 +0545

    feat(bumper): add arena, car, enemy car, docs (collectable)

---
+++ scripts/docs_spawn_path.gd
@@ -0,0 +1,16 @@
+extends Path2D
+
+var docs = preload("res://scenes/docs.tscn")
+
+@onready var spawnpoint: PathFollow2D = $spawnpoint
+
+func spawn_docs():
+	for i in range(6):
+		var docs_instance = docs.instantiate()
+		spawnpoint.progress_ratio = randf()
+		docs_instance.global_position = spawnpoint.global_position 
+		add_child(docs_instance)
+
+func _ready() -> void:
+	randomize()
+	spawn_docs()
