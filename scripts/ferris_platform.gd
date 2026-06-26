600d9b9ebeeee75dde2443c901aa611ef8b57ab1
dofiki <dixitregmiofficial@gmail.com>
2026-06-26 13:51:11 +0545

    add assets for level1 and level2

--- scripts/ferris_platform.gd
+++ scripts/ferris_platform.gd
@@ -6,11 +6,13 @@ var doc_value: int = 1
 @onready var coll_shape: CollisionShape2D = $CollisionShape2D
 
 func spawn_doc() -> void:
+	
 	if (randi_range(0, 10) < 5):
 		print("Spawning doc")
 
 		GameController.doc_spawned(doc_value)
 		var d = doc.instantiate()
+		d.z_index= 1
 		d.position = doc_marker.position
 		add_child(d)
 
--- scripts/mini_ferris.gd
+++ scripts/mini_ferris.gd
@@ -4,7 +4,7 @@ extends Node2D
 @onready var container: Sprite2D = $container
 @onready var wheel: Sprite2D = %ferris_wheel
 
-var plat : PackedScene = preload("res://scenes/ferris_platform.tscn")
+var plat : PackedScene = preload("res://scenes/ferris_platform2.tscn")
 var plats : Array[AnimatableBody2D] = []
 
 @export var wheel_speed: float = 0.2
@@ -12,7 +12,7 @@ var plats : Array[AnimatableBody2D] = []
 @export var speed : float = 0.5
 @export var radius: float = 100
 @export var angle_off : float = 0.0
-@export var spoke_color: Color = Color.RED
+@export var spoke_color: Color = Color.DARK_SLATE_GRAY
 @export var platform_color : Color = Color.GREEN
 
 func spawn_platforms() -> void:
@@ -33,12 +33,22 @@ func rotate_plats(delta: float) -> void:
 		var angle = TAU * float(i) / count + angle_off
 		p.position = Vector2(cos(angle), sin(angle)) * radius	
 
-
-
-
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	spawn_platforms()
+	
+func draw_spokes() -> void:
+	for i in range(count):
+		draw_line(
+			pillar.position,
+			plats[i].position,
+			spoke_color,
+			5.0 # line width
+		)
+
+func _draw() -> void:
+	draw_spokes()
+
 
 func _physics_process(delta: float) -> void:
 	rotate_plats(delta)
